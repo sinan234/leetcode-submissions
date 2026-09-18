081c115613fec4893af456c8ba29e498b6ef86ff
Mohammad Sinan M P <89132343+sinan234@users.noreply.github.com>
2026-09-18 12:28:22 +0530

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+
+        int left =0;
+        int right = numbers.Length-1;
+
+        while(left<right){
+            if(numbers[left] + numbers[right] == target){
+                res[0] = left+1;
+                res[1] = right+1;
+                break;
+            }
+            else if(numbers[left] + numbers[right] < target){
+                left ++;
+            }
+            else if(numbers[left] + numbers[right] > target){
+                right--;
+            }
+        }
+
+        return res;
+    }
+}
