77a40fc4a33c5d1a00943e46eaafe5dbd27d56af
Mohammad Sinan M P <89132343+sinan234@users.noreply.github.com>
2026-09-18 12:19:48 +0530

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+
+        Dictionary<int, int> d = new Dictionary<int, int>();
+
+        for(int i=0;i<numbers.Length;i++){
+            if(d.ContainsKey(target-numbers[i])){
+                res[0]= d[target-numbers[i]]+1;
+                res[1] = i+1;
+                break;
+            }
+            else{
+                if(!d.ContainsKey(numbers[i])){
+            d[numbers[i]] = i;
+
+            }
+            }
+        }
+        // int left =0;
+
+        // while (left < numbers.Length-1){
+        //     int right = left +1;
+
+        //     while(right < numbers.Length ){
+        //         if(numbers[left] + numbers[right] == target){
+        //             res[0] = left +1;
+        //             res[1] = right +1;
+        //         }
+
+        //         right ++;
+        //     }
+
+        //     left++;
+        // }
+
+        return res;
+    }
+}
